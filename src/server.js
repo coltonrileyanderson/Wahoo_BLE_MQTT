810d1afe19796b1454c2f40370ff8ab25561f451
Colton Anderson <coltonanderson@Coltons-MacBook-Pro.local>
2022-04-06 12:22:03 -0600

    first commit

---
+++ src/server.js
@@ -0,0 +1,12 @@
+const app = require("./app");
+const port = 4000;
+
+app.listen(port, () => console.log(`Server Listening on ${port}`));
+
+
+// var http = require('http');
+// http.createServer(function (req, res) {
+//   res.writeHead(200, {'Content-Type': 'text/plain'});
+//   res.end('Hello World\n');
+// }).listen(1337, '127.0.0.1');
+// console.log('Server running at http://127.0.0.1:1337/');
